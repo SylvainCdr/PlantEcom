f0a05873a7f7bd3e3d5b38743f70673eb8076656
sly <sylvaincadoret@hotmail.com>
2024-01-04 14:22:14 +0100

    ➕ make_entities

---
+++ src/Entity/OrdersDetails.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrdersDetailsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrdersDetailsRepository::class)]
+class OrdersDetails
+{
+
+    // On supprime l'id car on n'en a pas besoin
+
+    // #[ORM\Id]
+    // #[ORM\GeneratedValue]
+    // #[ORM\Column]
+    // private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+
+    // On ajoute l'attribut ORM/Id afin de définir la clé primaire
+    #[ORM\Id]
+    #[ORM\ManyToOne(inversedBy: 'ordersDetails')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Orders $orders = null;
+
+    // On ajoute l'attribut ORM/Id afin de définir la clé primaire
+    #[ORM\Id]
+    #[ORM\ManyToOne(inversedBy: 'ordersDetails')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Products $products = null;
+
+    // On supprime le getter et le setter de l'id car on n'en a pas besoin
+
+    // public function getId(): ?int
+    // {
+    //     return $this->id;
+    // }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getOrders(): ?Orders
+    {
+        return $this->orders;
+    }
+
+    public function setOrders(?Orders $orders): static
+    {
+        $this->orders = $orders;
+
+        return $this;
+    }
+
+    public function getProducts(): ?Products
+    {
+        return $this->products;
+    }
+
+    public function setProducts(?Products $products): static
+    {
+        $this->products = $products;
+
+        return $this;
+    }
+}
