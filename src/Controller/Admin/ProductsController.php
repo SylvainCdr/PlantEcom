d68bd706487c304966f4bdd3699318c9917c9cb0
sly <sylvaincadoret@hotmail.com>
2024-02-01 19:51:15 +0100

    ✅ add products, img & display OK

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -141,9 +142,38 @@ class ProductsController extends AbstractController
 
 
 
-    #[Route('/suppression/{id}', name: 'delete')]
-    public function delete(Products $product): Response
+    #[Route('/suppression/image/{id}', name: 'delete_image', methods: ['DELETE'])]
+    public function deleteImage(Images $image, Request $request, EntityManagerInterface $em, PictureService $pictureService): JsonResponse
     {
-        return $this->render('admin/products/index.html.twig');
-    }
+
+          //On vérifie si l'utilisateur peut supprimer avec le Voter
+        // $this->denyAccessUnlessGranted('PRODUCT_DELETE', $product);
+        // return $this->render('admin/products/index.html.twig');
+
+        //on récupère le contenu de la requête
+$data = json_decode($request->getContent(), true);
+
+if ($this->isCsrfTokenValid('delete' . $image->getId(), $data['_token'])) {
+// le token est valide 
+// On récupère le nom de l'image
+$nom = $image->getName();
+
+if($pictureService->delete($nom, 'products', 300, 300)){
+    //On supprime l'image de la base de données
+    $em->remove($image);
+    $em->flush();
+   
+    return new JsonResponse(['success' => true], 200);
 }
+
+// la suppression a échoué
+return new JsonResponse(['error' => 'Erreur de suppression'], 400);
+
+
+
+}
+return new JsonResponse(['error' => 'Token invalide'], 400);
+    
+}
+
+    }
